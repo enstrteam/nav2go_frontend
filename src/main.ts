e1dbd5fe1eeb5ce42c158252307567602765e56e
enstrteam <enstrteam@gmail.com>
2025-03-01 11:44:34 +0300

    Add telegram sdk

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,40 @@ import router from "./router";
 
 const pinia = createPinia();
 
-createApp(App)
-  .use(router)
-  .use(pinia)
-  .use(createYmaps({ apikey: "6dc0eaa2-b378-4e77-9999-1cfdf124c037" }))
-  .mount("#app");
+function loadTelegramSDK(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      resolve(window.Telegram.WebApp);
+    } else {
+      const script = document.createElement("script");
+      script.src = "https://telegram.org/js/telegram-web-app.js";
+      script.async = true;
+
+      script.onload = () => {
+        resolve(window.Telegram.WebApp);
+      };
+      script.onerror = () => {
+        reject(new Error("Failed to load Telegram WebApp SDK"));
+      };
+
+      document.head.appendChild(script);
+    }
+  });
+}
+
+loadTelegramSDK()
+  .then(() => {
+    createApp(App)
+      .use(router)
+      .use(pinia)
+      .use(createYmaps({ apikey: "6dc0eaa2-b378-4e77-9999-1cfdf124c037" }))
+      .mount("#app");
+  })
+  .catch((error) => {
+    console.error("Error loading Telegram SDK:", error);
+    createApp(App)
+      .use(router)
+      .use(pinia)
+      .use(createYmaps({ apikey: "6dc0eaa2-b378-4e77-9999-1cfdf124c037" }))
+      .mount("#app");
+  });
