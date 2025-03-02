6bc94f6e7638d66b2a8e0cfcf4abd3f0daea619b
enstrteam <enstrteam@gmail.com>
2025-03-02 18:12:53 +0300

    Fix home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <h1 class="title">Nav2Go</h1>
       <h2 class="subtitle">Алтай</h2>
       <p class="description">
-        <span v-if="user.id">Привет! {{user.id, user.username, user.firstname }} <br></span>
+        <span v-if="user.id">Привет! {{user.id, user.username, user.firstname }} </span>
           Выбирай впечатления <br>и создавай уникальные воспоминания
       </p>
     </header>
